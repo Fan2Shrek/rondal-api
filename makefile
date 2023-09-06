53e1fa4625ea8f275ade85c9da7d55dc4f8e8585
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-09-06 22:23:38 +0200

    Add provider entity

---
+++ makefile
@@ -0,0 +1,4 @@
+DOCKER_ENABLED=1
+
+include .boing/makes/boing.mk
+include .boing/makes/symfony.mk
---
+++ src/Entity/Provider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ProviderRepository;
+
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Post(),
+    ]
+)]
+#[ORM\Entity(repositoryClass: ProviderRepository::class)]
+class Provider
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private int $id;
+
+    #[ORM\Column(length: 255)]
+    private string $name;
+
+    #[ORM\Column(length: 255)]
+    private string $url;
+
+    public function getId(): ?int
+    {
+        return $this->id ?? null;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): static
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
