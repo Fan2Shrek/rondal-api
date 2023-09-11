0a67bf62ef36e23f99b3bd83acc7019d64c477b3
Fan2Shrek <pierre27.ambroise@gmail.com>
2023-09-11 17:17:37 +0200

    Made call to providers

--- src/Services/PriceFinder.php
+++ src/Services/PriceFinder.php
@@ -2,13 +2,24 @@
 
 namespace App\Services;
 
+use App\Services\Exception\PriceNotFoundException;
 use App\Services\Interfaces\PriceFinderInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class PriceFinder implements PriceFinderInterface
 {
-    public function findByReponse(ResponseInterface $responseInterface): int
+    public function findByReponse(ResponseInterface $response): int
     {
+        $content = $response->getContent();
+
+        if (!str_contains($content, '€')) {
+            throw new PriceNotFoundException(sprintf("Price was not found in the %s request", $response->getInfo()['url']));
+        }
+
+        \preg_match('/<[^>]*>\s*([\d,.]+)\s*€\s*<[^>]*>/', $content, $matches);
+
+        dump($matches[0]);
+
         return 1;
     }
 }
